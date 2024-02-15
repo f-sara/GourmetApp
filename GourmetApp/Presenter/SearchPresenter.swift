457cf878c789b90cf8fc911980bf4345529d9155
f-sara <jyujyusa.68@gmail.com>
2024-02-15 17:59:44 +0900

    feat: SearchPresetnerを作成

--- GourmetApp/Presenter/SearchPresenter.swift
+++ GourmetApp/Presenter/SearchPresenter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+protocol SearchPresenterInput: AnyObject {
+    func tappedGenre(latitude: Double, longitude: Double, keyword: String)
+}
+
+protocol SearchPresenterOutput: AnyObject {
+    func showSearchResult(_ data: RestaurantDataModel)
+    func showError(error: Error)
+}
+
+final class SearchPresenter {
+    private weak var output: SearchPresenterOutput!
+    private var apiClient: APIClient!
+
+    init(output: SearchPresenterOutput!, apiClient: APIClient!, restaurantDataModel: [Shop]) {
+        self.output = output
+        self.apiClient = apiClient
+    }
+}
+
+extension SearchPresenter: SearchPresenterInput {
+    func tappedGenre(latitude: Double, longitude: Double, keyword: String) {
+        apiClient.fetchRestaurantData(latitude: latitude, longitude: longitude, keyword: keyword, range: "3") { [weak self]  result in
+            guard let self = self else {
+                return
+            }
+            switch result {
+            case .success(let restaurantData):
+                self.output?.showSearchResult(restaurantData)
+            case .failure(let error):
+                self.output?.showError(error: error)
+            }
+        }
+    }
+    
+
+}
