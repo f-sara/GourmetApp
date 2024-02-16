53cc32d7c15f0af15bcb6fa0367c401796dab8ff
f-sara <jyujyusa.68@gmail.com>
2024-02-16 10:12:53 +0900

    refactor: 影を描写する処理を切り分け

--- GourmetApp/View/CollectionViewCell/GenreCollectionViewCell.swift
+++ GourmetApp/View/CollectionViewCell/GenreCollectionViewCell.swift
@@ -7,16 +7,49 @@
 
 import UIKit
 
+// MARK: - GenreCollectionViewCell
+
 class GenreCollectionViewCell: UICollectionViewCell {
+
+
+    // MARK: Private Properties
+
+    private let genreImages = [
+        UIImage(named: "japaneseFood"),
+        UIImage(named: "westernFood"),
+        UIImage(named: "chineseFood"),
+        UIImage(named: "ramen"),
+        UIImage(named: "cafe"),
+        UIImage(named: "izakaya")
+    ]
+    private let genreName =  ["和食", "洋食", "中華", "ラーメン", "カフェ", "居酒屋"]
+
+
+    // MARK: IBOutlets
 
     @IBOutlet weak var genreImage: UIImageView!
     @IBOutlet weak var genreLabel: UILabel!
 
-    private let genreImages = [UIImage(named: "japaneseFood"), UIImage(named: "westernFood"), UIImage(named: "chineseFood"), UIImage(named: "ramen"), UIImage(named: "cafe"), UIImage(named: "izakaya")]
-    private let genreName =  ["和食", "洋食", "中華", "ラーメン", "カフェ", "居酒屋"]
+
+    // MARK: View Life-Cycle Methods
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpShadow()
+    }
+
+
+    // MARK: Internal Methods
+
+    func setup(index: Int) {
+        genreImage.image = genreImages[index]
+        genreLabel.text = genreName[index]
+    }
+
+
+    // MARK: Private Methods
+
+    private func setUpShadow() {
         clipsToBounds = false
         backgroundColor = .white
         layer.cornerRadius = 4.0
@@ -25,10 +58,4 @@ class GenreCollectionViewCell: UICollectionViewCell {
         layer.shadowRadius = 1.0
         layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
     }
-
-    func setup(index: Int) {
-        genreImage.image = genreImages[index]
-        genreLabel.text = genreName[index]
-    }
-
 }
